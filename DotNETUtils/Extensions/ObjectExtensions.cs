537f0c5a5b0462ee34455ee413d4de641eea1228
Daniel Neel <dneelyep@gmail.com>
2015-05-12 23:38:38 -0400

    Add code to recreate the SQL IN keyword in .net code.

---
+++ DotNETUtils/Extensions/ObjectExtensions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DotNETUtils.Extensions
+{
+    public static class ObjectExtensions
+    {
+        public static bool In<T>(this T tObject, params T[] atObjects)
+        {
+            return atObjects.Contains(tObject);
+        }
+    }
+}
