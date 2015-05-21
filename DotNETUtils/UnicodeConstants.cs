f6d41d7c59a652dcea9620a087b95e4182f124e1
Daniel Neel <dneelyep@gmail.com>
2015-05-20 22:27:18 -0400

    Add constants to represent a few Unicode characters.

---
+++ DotNETUtils/UnicodeConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace DotNETUtils
+{
+    /// <summary>
+    /// A collection of constants representing Unicode characters.
+    /// </summary>
+    public static class UnicodeConstants
+    {
+        public const string UP_POINTING_TRIANGLE = "▲";
+        public const string DOWN_POINTING_TRIANGLE = "▼";
+    }
+}
